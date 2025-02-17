3ee2bb7bd7859bfc9847a13fb8cbf3bd7996cc28
ShaoKuy <iqtanawad@gmail.com>
2025-02-17 19:51:05 +0700

    save

---
+++ Assets/Scripts/Input/InputReader.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.InputSystem;
+using static Control;
+
+[CreateAssetMenu(fileName = "New Input Reader", menuName = "Input/Input Reader")]
+public class InputReader : ScriptableObject, IPlayerActions
+{
+    public event Action<Vector2> MoveEvent;
+    public event Action<bool> PrimaryFireEvent;
+
+    private Control controls;
+
+    private void OnEnable()
+    {
+        if (controls == null)
+        {
+            controls = new Control();
+            controls.Player.SetCallbacks(this);
+        }
+        controls.Player.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.Player.Disable();
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        MoveEvent?.Invoke(context.ReadValue<Vector2>());
+    }
+
+    public void OnInteract(InputAction.CallbackContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnSprint(InputAction.CallbackContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnPrimaryFire(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            PrimaryFireEvent?.Invoke(true);
+        }
+        else if (context.canceled)
+        {
+            PrimaryFireEvent?.Invoke(false);
+        }
+    }
+}
